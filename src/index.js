1c585002f9d264dccf4e3126f2d5673192a2400f
ign0ramus <yanushkevich_dmitry@mail.ru>
2018-11-22 16:01:25 +0300

    implemented js logic for navigation list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './icon-font.css';
 import './scss/main.scss';
+
+function navListClickHandler(e) {
+    let target = e.target;
+    if (this.contains(target)) {
+        document.querySelector('.navigation__checkbox').checked = false;
+    }
+}
+
+document.querySelector('.navigation__list').addEventListener('click', navListClickHandler);
